2201bf7e14e59f526f5dbdb566ce7c8c29fb2277
Franco-Ever <francoever1998@gmail.com>
2020-09-24 19:26:32 -0400

    update

--- AvanceProyecto3/script.js
+++ AvanceProyecto3/script.js
@@ -14,3 +14,13 @@ jQuery('document').ready(function($){
     })
 
 })
+var fecha_actual=new Date();
+var dia=fecha_actual.getDate();
+var mes=fecha_actual.getMonth()+1;
+var anio=fecha_actual.getUTCFullYear();
+
+var hora=fecha_actual.getHours();
+var min=fecha_actual.getMinutes();
+var seg=fecha_actual.getSeconds();
+
+document.write('Hora y fecha de Bolivia: ' + dia +'/'+mes+'/'+anio+' '+hora+':'+min+':'+seg+' hrs.');
